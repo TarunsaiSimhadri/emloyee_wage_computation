8c0373b7522fc089aafc6e8a663014d997a6dd9e
Tarunsai Simhadri <tarunsimhadri23@gmil.com>
2024-07-18 22:49:03 +0530

    [Tarunsai] Add: function to calculate part time employee wage

--- employee_wage_computation.py
+++ employee_wage_computation.py
@@ -13,9 +13,10 @@ import random
 
 WAGE_PER_HOUR = 20  
 FULL_DAY_WORKING_HOURS = 8
+PART_TIME_WAGE = 15
+PART_TIME_WORKING_HOURS = 8
 
 def check_attendance():
-
 
     """
     description:
@@ -25,7 +26,6 @@ def check_attendance():
     return:
         employee_attendance
     """    
-
 
     attendance = random.randint(0, 1)
 
@@ -40,7 +40,6 @@ def check_attendance():
 
 def calculate_full_time_employee_wage_per_day(check_attendance):
 
-
     """
     description:
         This function is used to calculate employee wage for full time employees.
@@ -50,9 +49,31 @@ def calculate_full_time_employee_wage_per_day(check_attendance):
         employee_wage_per_day
     """ 
 
-    
+
     if check_attendance == 'present':
         employee_wage_per_day = WAGE_PER_HOUR*FULL_DAY_WORKING_HOURS
+
+    else:
+        employee_wage_per_day = 0
+
+    return employee_wage_per_day
+
+
+def calculate_part_time_employee_wage_per_day(check_attendance):
+
+
+    """
+    description:
+        This function is used to calculate part time employee wage per day.
+    parameters:
+        check_attendance - to check attendance and calculate wage
+    return:
+        employee_wage_per_day
+    """ 
+
+
+    if check_attendance == 'present':
+        employee_wage_per_day = PART_TIME_WORKING_HOURS*PART_TIME_WAGE
 
     else:
         employee_wage_per_day = 0
@@ -63,7 +84,8 @@ def calculate_full_time_employee_wage_per_day(check_attendance):
 def main():
     emp_attendance = check_attendance()
     emp_wage = calculate_full_time_employee_wage_per_day(emp_attendance)
-    
+    emp_wage_part_time = calculate_part_time_employee_wage_per_day(emp_attendance)
+
 
 if __name__ == '__main__':
     main()
