74b81fc386c4c7ee044a769ca6ad6fa91b1f1396
Tarunsai Simhadri <tarunsimhadri23@gmil.com>
2024-07-18 23:04:34 +0530

    [Tarunsai] Refactor: Code to write a Class Method to Compute Employee Wage

--- employee_wage_computation.py
+++ employee_wage_computation.py
@@ -11,94 +11,58 @@
 
 import random
 
-WAGE_PER_HOUR = 20  
-FULL_DAY_WORKING_HOURS = 8
-PART_TIME_WAGE = 15
-PART_TIME_WORKING_HOURS = 8
+class Employee:
 
+    FULL_DAY_HOURS = 8
+    PART_TIME_HOURS = 4
+    WORKING_DAYS_PER_MONTH = 20
+    MAX_WORKING_HOURS = 100
 
-def check_attendance():
-
-    """
-    description:
-        This function is used to check attendance.
-    parameters:
-        None
-    return:
-        employee_attendance
-    """    
-
-    attendance = random.randint(0, 1)
-
-    if attendance == 1:
-        employee_attendance = 'present'
-
-    else:
-        employee_attendance = 'absent'
+    def __init__(self, name, wage_per_hr):
+        self.emp_name = name
+        self.wage_per_hr = wage_per_hr
+        self.total_hrs_worked = 0
+        self.total_days_worked = 0
+        self.total_wage = 0    
     
-    return employee_attendance
-
-
-def employee_type_wage_per_day(employee_type):
-    """
-    description:
-        This function is used to find employee type and wage accordingly.
-    parameters:
-        employee_type - according to that type generate employee wage
-    return:
-        employee_wage_per_day, else : enter correct employee_type
-    """    
-
-    if employee_type == 1:
-        employee_wage_per_day = WAGE_PER_HOUR * FULL_DAY_WORKING_HOURS
-        return employee_wage_per_day
+    @staticmethod    
+    def get_attendance():
+        return random.randint(0, 2)
     
-    else:
-        employee_wage_per_day = PART_TIME_WAGE * PART_TIME_WORKING_HOURS
-        return employee_wage_per_day
-        
-   
-def calculate_employee_monthly_wage():
+    
+    def daily_wage(self):
+        emp_status = self.get_attendance()
+    
+        if emp_status == 0:
+            return 0,0
+        elif emp_status == 1: 
+            return self.wage_per_hr * self.FULL_DAY_HOURS, self.FULL_DAY_HOURS
+        elif emp_status == 2:
+            return self.wage_per_hr * self.PART_TIME_HOURS, self.PART_TIME_HOURS
+            
+    def monthly_wage(self):
+        while self.total_days_worked < self.WORKING_DAYS_PER_MONTH and self.total_hrs_worked < self.MAX_WORKING_HOURS:
+            wage, hours = self.daily_wage()
+            # print(wage)
+            self.total_wage += wage
+            self.total_hrs_worked += hours
+            self.total_days_worked += 1
 
-    """
-    description:
-        This function is used to calculate monthly wage.
-    parameters:
-        None
-    return:
-        employee_monthly wage
-    """
-    emp_atd_lst = []
-    emp_daily_wage =[]
-    working_days = 1
-    total_wage = 0
-    total_hours = 0
-    while working_days < 20 and total_hours <= 100:
-        emp_attendance = check_attendance()
-        emp_atd_lst.append(emp_attendance)
-        if emp_attendance == 'present':
-            emp_type = random.randint(0,1)
-            one_day_wage=employee_type_wage_per_day(emp_type)
-            emp_daily_wage.append(one_day_wage)
-            total_wage+=one_day_wage
-            total_hours+=8
-            if total_hours>100:
-                one_day_wage = emp_daily_wage[-1] / 2
-                emp_daily_wage[-1] = one_day_wage
-                total_wage = total_wage - one_day_wage
-        else:
-            emp_daily_wage.append(0)
-            total_wage+=0
-        working_days+=1
-    print(working_days, total_hours)
-    return total_wage, emp_atd_lst, emp_daily_wage
+    def get_emp_details(self):
+        print(f"Name: {self.emp_name}")
+        print(f"Total Wage: {self.total_wage}") 
+        print(f"Total Hours Worked: {self.total_hrs_worked}")
+        print(f"Total Days Worked: {self.total_days_worked}")
 
 
 def main():
-    total_wage, emp_atd_lst, emp_daily_wage = calculate_employee_monthly_wage()
-    print(f'Employee 20 days attendance is {emp_atd_lst}')
-    print(f'Employee daily wage for 20 days is {emp_daily_wage}')
-    print(f'Total monthly wage of employee is {total_wage}')
+    emp1 = Employee("tarun", 20)
+    emp2 = Employee("varun", 30)
+    emp3 = Employee("kamal", 20)
+
+    emp1.monthly_wage()
+    emp2.monthly_wage()
+    emp3.monthly_wage()
 
 
 if __name__ == '__main__':
